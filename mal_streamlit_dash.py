819a5e23c945c2cf2f939eacb900fca11f88cfcc
jamesinjune <jamesli.jcll@gmail.com>
2024-09-24 15:14:10 -0700

    Update mal_streamlit_dash.py
    - Fixed spacing

--- mal_streamlit_dash.py
+++ mal_streamlit_dash.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 import streamlit as st
+
+
 # Page Configuration
 st.set_page_config(layout='wide', page_title='SeasonalMAL')
 
